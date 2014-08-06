8b0c22d89b91332e7e541f5191d914ab116e1ae9
Maciej Schmidt <macsch15@gmail.com>
2014-08-06 14:32:18 +0200

    Tests config

---
+++ library/Tricolore/Tests/Bootstrap/Bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+use Tricolore\Autoloader;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . 
+    DIRECTORY_SEPARATOR . '..' . 
+    DIRECTORY_SEPARATOR . 'Autoloader.php';
+
+Autoloader::register();
