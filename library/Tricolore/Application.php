62111b4f66294bcb8cac7c42632c34389f7e2518
Maciej Schmidt <macsch15@gmail.com>
2014-08-14 17:52:20 +0200

    Application space

--- library/Tricolore/Application.php
+++ library/Tricolore/Application.php
@@ -6,6 +6,13 @@ use Tricolore\View\View;
 class Application
 {
     /**
+     * Application options
+     * 
+     * @var array
+     */
+    private static $options;
+
+    /**
      * Register application and services
      *
      * @param array $options
@@ -13,6 +20,70 @@ class Application
      */
     public static function register(array $options)
     {
+        self::$options = $options;
+    }
 
+    /**
+     * Application instance
+     * 
+     * @return Tricolore\Application
+     */
+    public static function getInstance()
+    {
+        return new static();
+    }
+
+    /**
+     * Create path
+     * 
+     * @param string $path 
+     * @throws \Exception
+     * @return string
+     */
+    public static function createPath($path = null)
+    {
+        if(isset(self::$options['directory']) === false || self::$options['directory'] == null) {
+            throw new \Exception('Base directory are not defined in Application options');
+        }
+
+        if($path === null) {
+            return self::$options['directory'];
+        }
+
+        $path = str_replace(':', DIRECTORY_SEPARATOR, $path);
+
+        return self::$options['directory'] . $path;
+    }
+
+    /**
+     * Get application environment 
+     *  
+     * @return string
+     */
+    public function getEnv()
+    {
+        $available_environments = ['dev', 'prod'];
+
+        if(isset(self::$options['environment']) === false || self::$options['environment'] == null
+            || in_array(self::$options['environment'], $available_environments, true) === false
+        ) {
+            return 'prod';
+        }
+
+        return self::$options['environment'];
+    }
+
+    /**
+     * Get application version
+     * 
+     * @return string
+     */
+    public function getVersion()
+    {
+        if(isset(self::$options['version']) === false || self::$options['version'] == null) {
+            return 'undefined';
+        }
+
+        return self::$options['version'];
     }
 }
---
+++ library/Tricolore/Services/Services.php
@@ -0,0 +1,7 @@
+<?php
+namespace Tricolore\Services;
+
+class Services
+{
+
+}
