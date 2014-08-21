055dd2401a40f0e3942ba5459d9f6ec30484a97e
Maciej Schmidt <macsch15@gmail.com>
2014-08-21 18:30:09 +0200

    Updated Tricolore\Services

--- library/Tricolore/Services/ServiceLocator.php
+++ library/Tricolore/Services/ServiceLocator.php
@@ -21,7 +21,20 @@ abstract class ServiceLocator
             throw new ServicesException(sprintf('Service "%s" not exists.', $key));
         }
 
+        if(class_exists($service_map['service-locator'][$key]['class']) === false) {
+            throw new ServicesException(sprintf('Class "%s" not exists.', $service_map['service-locator'][$key]['class']));
+        }
+
         $service_load = new $service_map['service-locator'][$key]['class'];
+
+        if(isset($service_map['service-locator'][$key]['function']) === false) {
+            return $service_load;
+        }
+
+        if(method_exists($service_load, $service_map['service-locator'][$key]['function']) === false) {
+            throw new ServicesException(sprintf('Method "%s" in class "%s" not exists.', 
+                $service_map['service-locator'][$key]['function'], get_class($service_load)));
+        }
 
         return call_user_func([$service_load, $service_map['service-locator'][$key]['function']]);
     }
