8da62b9458a795421b8c2280f84eca13eafe3a4d
Maciej Schmidt <macsch15@gmail.com>
2014-08-26 23:46:03 +0200

    Tweaked tests bootstrap

--- library/Tricolore/Tests/Bootstrap/Bootstrap.php
+++ library/Tricolore/Tests/Bootstrap/Bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 use Tricolore\Autoloader;
+use Tricolore\Application;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . 
     DIRECTORY_SEPARATOR . '..' . 
     DIRECTORY_SEPARATOR . 'Autoloader.php';
 
 Autoloader::register();
+
+Application::register([
+    'directory' => __DIR__ . '/../../../..' . DIRECTORY_SEPARATOR,
+    'environment' => 'test'
+]);
