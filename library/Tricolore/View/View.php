a38b960156fd9041e7b0655fb174790265597ed7
Maciej Schmidt <macsch15@gmail.com>
2014-08-26 23:46:37 +0200

    Fix #1: Integrate Form component with Twig

--- library/Tricolore/View/View.php
+++ library/Tricolore/View/View.php
@@ -2,16 +2,13 @@
 namespace Tricolore\View;
 
 use Tricolore\Application;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Form\TwigRenderer;
+use Symfony\Bridge\Twig\Form\TwigRendererEngine;
+use Symfony\Component\Form\Extension\Csrf\CsrfProvider\DefaultCsrfProvider;
 
 class View
 {
-    /**
-     * Twig Loader
-     *  
-     * @var \Twig_Loader_Filesystem
-     */
-    private $loader;
-
     /**
      * Twig Environment
      *  
@@ -28,15 +25,27 @@ class View
     {
         \Twig_Autoloader::register();
 
-        $this->loader = new \Twig_Loader_Filesystem(Application::createPath('library:Tricolore:View:Templates'));
+        $twig_form_template_dir = Application::createPath('library:Symfony:Bridge:Twig:Resources:views:Form');
+
+        $loader = new \Twig_Loader_Filesystem([
+            Application::createPath('library:Tricolore:View:Templates'),
+            $twig_form_template_dir
+        ]);
 
         $in_dev = (Application::getInstance()->getEnv() === 'dev') ? true : false;
 
-        $this->environment = new \Twig_Environment($this->loader, [
+        $this->environment = new \Twig_Environment($loader, [
             'cache' => ($in_dev) ? Application::createPath('storage:twig') : false,
             'auto_reload' => ($in_dev) ?: false,
             'strict_variables' => ($in_dev) ?: false
         ]);
+
+        $form = new TwigRendererEngine(['form_div_layout.html.twig']);
+        $form->setEnvironment($this->environment);
+
+        $csrf_provider = new DefaultCsrfProvider('some_secret_token');
+
+        $this->environment->addExtension(new FormExtension(new TwigRenderer($form, $csrf_provider)));
 
         return $this->environment;
     }
