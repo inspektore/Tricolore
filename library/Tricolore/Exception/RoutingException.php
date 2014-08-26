3e3597591283d8e231708a23b4bfb553eb5ace07
Maciej Schmidt <macsch15@gmail.com>
2014-08-26 23:45:29 +0200

    Added some exceptions

---
+++ library/Tricolore/Exception/RoutingException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Tricolore\Exception;
+
+class RoutingException extends \Exception
+{
+
+}
---
+++ library/Tricolore/Exception/RuntimeException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Tricolore\Exception;
+
+class RuntimeException extends \Exception
+{
+
+}
