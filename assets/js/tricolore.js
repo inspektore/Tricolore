f175656a51da302d794966a24b8c31e6fec2f39f
Maciej Schmidt <macsch15@gmail.com>
2014-09-16 00:22:12 +0200

    Added active tab in navbar

---
+++ assets/js/tricolore.js
@@ -0,0 +1,11 @@
+$(function() {
+  var filter = $('.nav a').filter(function () {
+    return this.href == location.href;
+  });
+
+  filter.parent().addClass('active').siblings().removeClass('active');
+
+  $('.nav a').click(function () {
+    $(this).parent().addClass('active').siblings().removeClass('active');
+  });
+});
