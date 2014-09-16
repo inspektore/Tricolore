987d7cc80c65c29f0db09db2674515f71832ee0e
Maciej Schmidt <macsch15@gmail.com>
2014-09-17 00:39:08 +0200

    Added validator extension to forms

--- library/Tricolore/FormFactory/FormFactory.php
+++ library/Tricolore/FormFactory/FormFactory.php
@@ -4,6 +4,9 @@ namespace Tricolore\FormFactory;
 use Symfony\Component\Form\Forms;
 use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
 use Symfony\Component\Form\Extension\Csrf\CsrfProvider\DefaultCsrfProvider;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 
 class FormFactory
 {
@@ -14,12 +17,46 @@ class FormFactory
      */
     public function getFactory()
     {
+        return Forms::createFormFactoryBuilder()
+        ->addExtension($this->getCsrfExtension())
+        ->addExtension($this->getValidatorExtension())
+        ->addExtension($this->getHttpFoundationExtension())
+        ->getFormFactory();
+    }
+
+    /**
+     * CSRF extension
+     * 
+     * @return Symfony\Component\Form\Extension\Csrf\CsrfExtension
+     */
+    private function getCsrfExtension()
+    {
         $csrf_secret_token = 'some_secret_token';
 
         $csrf_provider = new DefaultCsrfProvider($csrf_secret_token);
 
-        return Forms::createFormFactoryBuilder()
-        ->addExtension(new CsrfExtension($csrf_provider))
-        ->getFormFactory();
+        return new CsrfExtension($csrf_provider);
+    }
+
+    /**
+     * Validator extension
+     * 
+     * @return Symfony\Component\Form\Extension\Validator\ValidatorExtension
+     */
+    private function getValidatorExtension()
+    {
+        $validator = Validation::createValidator();
+
+        return new ValidatorExtension($validator);
+    }
+
+    /**
+     * HTTP Foundation extension
+     * 
+     * @return Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension
+     */
+    private function getHttpFoundationExtension()
+    {
+        return new HttpFoundationExtension();
     }
 }
