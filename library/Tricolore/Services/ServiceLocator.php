4431a078473abbcce30ace2cb9ed53f055e6cf8c
Maciej Schmidt <macsch15@gmail.com>
2014-10-03 00:16:41 +0200

    ServiceLocator tweaks

--- library/Tricolore/Services/ServiceLocator.php
+++ library/Tricolore/Services/ServiceLocator.php
@@ -20,15 +20,7 @@ abstract class ServiceLocator
      */
     final public function get($key, array $arguments = [], $service_file = null)
     {
-        if($service_file === null) {
-            $service_map = Yaml::parse(Application::createPath('library:Tricolore:Services:ServicesMap:Map.yml'));
-        } else {
-            if(file_exists($service_file) === false) {
-                throw new AssetNotFound(sprintf('File: %s does not exists.', $service_file));
-            }
-
-            $service_map = Yaml::parse($service_file); 
-        }
+        $service_map = $this->parseServicesMap($service_file);
         
         if(isset($service_map['service-locator'][$key]) === false) {
             throw new ServicesException(sprintf('Service "%s" not exists.', $key));
@@ -38,7 +30,7 @@ abstract class ServiceLocator
             throw new ServicesException(sprintf('Class "%s" not exists.', $service_map['service-locator'][$key]['class']));
         }
 
-        $service_load = new $service_map['service-locator'][$key]['class'];
+        $service_load = $this->serviceClassLoad($service_map, $key);
 
         if(isset($service_map['service-locator'][$key]['function']) === false) {
             return $service_load;
@@ -49,6 +41,83 @@ abstract class ServiceLocator
                 $service_map['service-locator'][$key]['function'], $service_map['service-locator'][$key]['class']));
         }
 
-        return call_user_func_array([$service_load, $service_map['service-locator'][$key]['function']], $arguments);
+        if($this->isStatic($service_map, $key) === true) {
+            return $this->callStatic([$service_load, $service_map['service-locator'][$key]['function']], $arguments);
+        }
+
+        return $this->callDynamic([$service_load, $service_map['service-locator'][$key]['function']], $arguments);
+    }
+
+    /**
+     * Call dymanic
+     * 
+     * @param callable $callback
+     * @param array $arguments
+     * @return mixed
+     */
+    final private function callDynamic(callable $callback, array $arguments)
+    {
+        return call_user_func_array($callback, $arguments);
+    }
+
+    /**
+     * Call static
+     * 
+     * @param callable $callback
+     * @param array $arguments
+     * @return mixed
+     */
+    final private function callStatic(callable $callback, array $arguments)
+    {
+        return forward_static_call_array($callback, $arguments);
+    }
+
+    /**
+     * Is static
+     * 
+     * @param array $service_map
+     * @param string $key
+     * @return bool
+     */
+    final private function isStatic(array $service_map, $key)
+    {
+        return isset($service_map['service-locator'][$key]['static']) && $service_map['service-locator'][$key]['static'] === true;
+    }
+
+    /**
+     * Service class load
+     * 
+     * @param array $service_map
+     * @param string $key 
+     * @return mixed
+     */
+    final private function serviceClassLoad(array $service_map, $key)
+    {
+        if($this->isStatic($service_map, $key) === true) {
+            return $service_map['service-locator'][$key]['class'];
+        }
+
+        return new $service_map['service-locator'][$key]['class'];
+    }
+
+    /**
+     * Parse YAML file to raw array
+     * 
+     * @param string $service_file
+     * @return array
+     */
+    final private function parseServicesMap($service_file)
+    {
+        if($service_file === null) {
+            $service_map = Yaml::parse(Application::createPath('library:Tricolore:Services:ServicesMap:Map.yml'));
+        } else {
+            if(file_exists($service_file) === false) {
+                throw new AssetNotFound(sprintf('File: %s does not exists.', $service_file));
+            }
+
+            $service_map = Yaml::parse($service_file); 
+        }
+
+        return $service_map;
     }
 }
