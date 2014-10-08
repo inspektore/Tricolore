0055347cb151a43601770e51f4bcd17706e6c208
Maciej Schmidt <macsch15@gmail.com>
2014-10-09 00:02:19 +0200

    Changed DatabaseDriver to DatabaseAdapter #5

---
+++ library/Tricolore/DataSources/DatabaseAdapter.php
@@ -0,0 +1,40 @@
+<?php
+namespace Tricolore\DataSources;
+
+use Tricolore\Application;
+use Tricolore\Config\Config;
+use Tricolore\Exception\DatabaseException;
+
+class DatabaseAdapter
+{
+    /**
+     * Get database factory
+     * 
+     * @throws Tricolore\Exception\DatabaseException
+     * @return Tricolore\DataSources\PostgreSQL\DatabaseFactory
+     */
+    public function getDatabaseFactory()
+    {
+        $database_config = Config::all('Database');
+
+        $allowed_adapters = ['PostgreSQL'];
+
+        if(in_array($database_config['Adapter'], $allowed_adapters, true) === false) {
+            throw new DatabaseException(
+                sprintf('Adapter "%s" not found. Known adapters: %s', $database_config['Adapter'], implode(', ', $allowed_adapters)));
+        }
+
+        $driver_class = __NAMESPACE__ . '\\' . $database_config['Adapter'] . '\\DatabaseFactory';
+        $driver_class = new $driver_class;
+
+        if(Application::getInstance()->getEnv() === 'test') {
+            if(Application::getInstance()->inTravis() === true) {
+                $database_config['Adapter'] = 'Travis';
+            } else {
+                $database_config['Adapter'] = 'Tests';
+            }
+        }
+
+        return $driver_class->connection($database_config['Configuration'][$database_config['Adapter']]);
+    }
+}
