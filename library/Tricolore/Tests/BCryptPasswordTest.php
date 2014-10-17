91c030ff8adf91d877b2b7a41eca7d806877dbcd
Maciej Schmidt <macsch15@gmail.com>
2014-10-17 15:38:09 +0200

    Covered BCrypt with tests

---
+++ library/Tricolore/Tests/BCryptPasswordTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace Tricolore\Tests;
+
+use Tricolore\Security\PasswordEncoder\BCryptEncoder;
+
+class BCryptPasswordTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBcryptHasher()
+    {
+        $my_password = 'Example super password!';
+        $salt = 'Lorem ipsum Anim ullamco magna Duis non ut exercitation in reprehenderit';
+        $cost = 10;
+
+        $expected = '$2y$10$TG9yZW0gaXBzdW0gQW5pbOs6IDnjToYF4xRiNTmg5hBu8BljuL2Pu';
+        $actual = BCryptEncoder::passwordHash($my_password, [
+            'salt' => $salt,
+            'cost' => $cost
+        ]);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testBcryptHasherLen()
+    {
+        $my_password = 'Example super password!';
+        $hash = BCryptEncoder::passwordHash($my_password);
+
+        $expected = 60;
+        $actual = strlen($hash);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testPasswordVerify()
+    {
+        $my_password = 'Example super password!';
+        $hash = BCryptEncoder::passwordHash($my_password);
+
+        $this->assertTrue(BCryptEncoder::passwordVerify($my_password, $hash));
+    }
+
+    public function testAlgo()
+    {
+        $my_password = 'Example super password!';
+        $hash = BCryptEncoder::passwordHash($my_password);
+
+        $expected = 'bcrypt';
+        $actual = BCryptEncoder::passwordInfo($hash)['algoName'];
+
+        $this->assertEquals($expected, $actual);
+    }
+}
