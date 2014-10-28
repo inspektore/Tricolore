6868d39ad9295e7c4a41b4b2a6cca556f756de1d
Maciej Schmidt <macsch15@gmail.com>
2014-10-28 07:00:16 +0100

    WIP: AdminCP Auth

---
+++ library/Tricolore/Controller/Backend/Auth.php
@@ -0,0 +1,34 @@
+<?php
+namespace Tricolore\Controller\Backend;
+
+use Tricolore\Controller\FormTypes\Backend\AuthType;
+use Tricolore\Services\ServiceLocator;
+use Symfony\Component\HttpFoundation\Request;
+
+class Auth extends ServiceLocator
+{
+    /**
+     * @Route('/admincp/auth', name="admincp_auth")
+     */
+    public function process()
+    {
+        $form = $this->get('form')->create(new AuthType(), [
+            'translator' => $this->get('translator')
+        ]);
+
+        $form->handleRequest(Request::createFromGlobals());
+
+        $validated = false;
+
+        if($form->isSubmitted() === true && $form->isValid() === true) {
+            $validated = true;
+        }
+
+        $render = [
+            'form' => $form->createView(),
+            'is_valid' => $validated
+        ];
+
+        return $this->get('view')->display('Actions/Backend', 'Auth', $render);
+    }
+}
---
+++ library/Tricolore/Controller/FormTypes/Backend/AuthType.php
@@ -0,0 +1,44 @@
+<?php
+namespace Tricolore\Controller\FormTypes\Backend;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AuthType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('Login', 'text', [
+            'constraints' => [
+                new Assert\NotBlank()
+            ],
+
+            'attr' => [
+                'placeholder' => $options['data']['translator']->trans('Email address or username')
+            ]
+        ])
+        ->add('Password', 'password', [
+            'constraints' => [
+                new Assert\NotBlank()
+            ],
+
+            'attr' => [
+                'placeholder' => $options['data']['translator']->trans('Password')
+            ]
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'auth';
+    }
+}
