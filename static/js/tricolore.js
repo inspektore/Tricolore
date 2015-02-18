8d195fcceee38d7d0a918746e37fba0fae71dc38
Maciej Schmidt <macsch15@gmail.com>
2015-02-19 00:45:39 +0100

    Moved LESS files to proper dir

--- static/js/tricolore.js
+++ static/js/tricolore.js
@@ -19,3 +19,36 @@
     $(this).parent().addClass('active').siblings().removeClass('active');
   });
 }();
+
++function searchInLoadedClasses()
+{
+  "use strict";
+
+  $("#search-loaded-classes").keyup(function () {
+    var filter = $(this).val();
+
+    $("ul li#loaded-class-node").each(function () {
+      var search = $(this).text().search(new RegExp(escapeRegExp(filter), "i"));
+
+      if (search < 0) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    });
+
+    var founded_classes = $('li#loaded-class-node').filter(':visible').length;
+
+    if (founded_classes == 0) {
+      $('#class-not-found').show();
+    } else {
+      $('#class-not-found').hide();
+    }
+
+    $('#loaded-classes-count').text(founded_classes);
+  });
+}();
+
+function escapeRegExp(string) {
+  return string.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
