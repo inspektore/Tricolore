0d2f32578e8fa695348fadbd00d46f5c35f4753e
Maciej Schmidt <macsch15@gmail.com>
2015-03-01 07:40:52 +0100

    Test getEnv() and cleanup

--- app/Tricolore/Tests/TwigIntegrationTest.php
+++ app/Tricolore/Tests/TwigIntegrationTest.php
@@ -5,58 +5,57 @@ use Tricolore\Config\Config;
 
 class TwigIntegrationTest extends \PHPUnit_Framework_TestCase
 {
+    private function getView()
+    {
+        return $this->getMockForAbstractClass('Tricolore\Services\ServiceLocator')
+        ->get('view');
+    }
+
     public function testAssetFunction()
     {
-        $service_locator = $this->getMockForAbstractClass('Tricolore\Services\ServiceLocator');
-        $service_view = $service_locator->get('view');
-
         $expected = 'http://localhost/Tricolore/tests/' . Config::key('directory.assets') . '/css/foo';
-        $actual = $service_view->display(null, 'TestAsset', [], true);
+        $actual = $this->getView()->display(null, 'TestAsset', [], true);
 
         $this->assertEquals($expected, $actual);
     }
 
     public function testConfigFunction()
     {
-        $service_locator = $this->getMockForAbstractClass('Tricolore\Services\ServiceLocator');
-        $service_view = $service_locator->get('view');
-
         $expected = 'en';
-        $actual = $service_view->display(null, 'TestConfig', [], true);
+        $actual = $this->getView()->display(null, 'TestConfig', [], true);
 
         $this->assertEquals($expected, $actual);
     }
 
     public function testAppGlobal()
     {
-        $service_locator = $this->getMockForAbstractClass('Tricolore\Services\ServiceLocator');
-        $service_view = $service_locator->get('view');
-
         $expected = 'test';
-        $actual = $service_view->display(null, 'TestAppGlobal', [], true);
+        $actual = $this->getView()->display(null, 'TestAppGlobal', [], true);
 
         $this->assertEquals($expected, $actual);
     }
 
     public function testUrlFunction()
     {
-        $service_locator = $this->getMockForAbstractClass('Tricolore\Services\ServiceLocator');
-        $service_view = $service_locator->get('view');
-
         $expected = 'http://localhost/Tricolore/tests/index.php?/my/test';
-        $actual = $service_view->display(null, 'TestUrl', [], true);
+        $actual = $this->getView()->display(null, 'TestUrl', [], true);
 
         $this->assertEquals($expected, $actual);
     }
 
     public function testUrlFunctionNoRoute()
     {
-        $service_locator = $this->getMockForAbstractClass('Tricolore\Services\ServiceLocator');
-        $service_view = $service_locator->get('view');
-
         $expected = 'http://localhost/Tricolore/tests';
-        $actual = $service_view->display(null, 'TestUrlNotRoute', [], true);
+        $actual = $this->getView()->display(null, 'TestUrlNotRoute', [], true);
 
         $this->assertEquals($expected, $actual);
+    }
+
+    public function testTwigEnvironmentInstance()
+    {
+        $expected = 'Twig_Environment';
+        $actual = $this->getView()->getEnv();
+
+        $this->assertInstanceOf($expected, $actual);
     }
 }
