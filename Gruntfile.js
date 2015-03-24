1e8f37992f272f60f747a350e0296764813286f7
Maciej Schmidt <macsch15@gmail.com>
2015-03-24 16:41:18 +0100

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+        },
+        files: {
+          "./static/css/ui.css":"./static/less/ui.less"
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: [
+          './static/less/*.less',
+          './static/less/*/*.less',
+          './static/less/*/*/*.less'
+        ],  //watched files
+        tasks: ['less'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
