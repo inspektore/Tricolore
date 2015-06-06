68eaddacefe0268cb410967e59ee3d3d2bacbeba
Maciej Schmidt <macsch15@gmail.com>
2015-06-07 01:54:43 +0200

    Cleanup

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php') === false) {
-    echo 'You must set up the project dependencies. 
-        <a href="https://github.com/Macsch15/Tricolore#downloading-and-installing-dependencies">Help</a>';
-
-    exit(1);
-}
-
 use Tricolore\Foundation\Application;
 
 require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
