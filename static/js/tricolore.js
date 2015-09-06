2ca76cc504262b114931b89f439071750a4df171
Maciej Schmidt <macsch15@gmail.com>
2015-09-06 12:26:25 +0200

    Code cleanup

--- static/js/tricolore.js
+++ static/js/tricolore.js
@@ -1,21 +1,15 @@
 +function registerBootstrapUtils()
 {
-  "use strict";
-
   $('[data-toggle="tooltip"]').tooltip({container: 'body'});
 }();
 
 +function destroyAllFlashMessages()
 {
-  "use strict";
-
   $('div.flash-message').not('.alert-important').delay(2000).slideUp(100);
 }();
 
 +function setUpNotificationPopover()
 {
-  "use strict";
-
   $('#notification-popover').popover({
     container: 'body',
     html : true,
