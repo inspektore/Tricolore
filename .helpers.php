987ed3da9adef5917b51314dfc24839f6ccbd646
Maciej Schmidt <macsch15@gmail.com>
2015-10-09 15:26:07 +0200

    make sure to helpers functions does not exists

--- .helpers.php
+++ .helpers.php
@@ -1,73 +1,81 @@
 <?php
 use Tricolore\Exception\RuntimeException;
 
-/**
- * Starts with
- * 
- * @param string $condition
- * @param string $from
- * @param int $length
- * @param bool $ignore_whitespaces
- * @return bool
- */
-function startsWith($condition, $from, $length = 1, $ignore_whitespaces = false)
-{
-    if ($ignore_whitespaces === true) {
-        $from = ltrim($from);
-    }
+if (function_exists('startsWith') === false) {
+    /**
+     * Starts with
+     * 
+     * @param string $condition
+     * @param string $from
+     * @param int $length
+     * @param bool $ignore_whitespaces
+     * @return bool
+     */
+    function startsWith($condition, $from, $length = 1, $ignore_whitespaces = false)
+    {
+        if ($ignore_whitespaces === true) {
+            $from = ltrim($from);
+        }
 
-    if (substr($from, 0, $length) === $condition) {
-        return true;
-    }
+        if (substr($from, 0, $length) === $condition) {
+            return true;
+        }
 
-    return false;
+        return false;
+    }
 }
 
-/**
- * Starts with
- * 
- * @param string $condition
- * @param string $from
- * @param int $length
- * @param bool $ignore_whitespaces
- * @return bool
- */
-function endsWith($condition, $from, $length = 1, $ignore_whitespaces = false)
-{
-    if ($ignore_whitespaces === true) {
-        $from = rtrim($from);
-    }
+if (function_exists('endsWith') === false) {
+    /**
+     * Starts with
+     * 
+     * @param string $condition
+     * @param string $from
+     * @param int $length
+     * @param bool $ignore_whitespaces
+     * @return bool
+     */
+    function endsWith($condition, $from, $length = 1, $ignore_whitespaces = false)
+    {
+        if ($ignore_whitespaces === true) {
+            $from = rtrim($from);
+        }
 
-    if (substr($from, -$length) === $condition) {
-        return true;
-    }
+        if (substr($from, -$length) === $condition) {
+            return true;
+        }
 
-    return false;
+        return false;
+    }
 }
 
-/**
- * under_score to StudlyCaps
- * 
- * @param string $text 
- * @return string
- */
-function underscoreToStudlyCaps($text)
-{
-    return str_replace(' ', '', ucwords(str_replace('_', ' ', $text)));
+if (function_exists('underscoreToStudlyCaps') === false) {
+    /**
+     * under_score to StudlyCaps
+     * 
+     * @param string $text 
+     * @return string
+     */
+    function underscoreToStudlyCaps($text)
+    {
+        return str_replace(' ', '', ucwords(str_replace('_', ' ', $text)));
+    }
 }
 
-/**
- * Redirect
- * 
- * @param string $target
- * @throws Tricolore\Exception\RuntimeException
- * @return void
- */
-function redirect($target)
-{
-    if (headers_sent() === true) {
-        throw new RuntimeException('Headers already sent.');
-    }
+if (function_exists('redirect') === false) {
+    /**
+     * Redirect
+     * 
+     * @param string $target
+     * @throws Tricolore\Exception\RuntimeException
+     * @return void
+     */
+    function redirect($target)
+    {
+        if (headers_sent() === true) {
+            throw new RuntimeException('Headers already sent.');
+        }
 
-    header(sprintf('Location: %s', $target));
+        header(sprintf('Location: %s', $target));
+    }
 }
