7883c9a19e4ae2951e6073a86de7325d3417b882
Macsch15 <macsch15@gmail.com>
2016-04-22 18:09:54 +0200

    Added some validation to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,5 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@home')->name('core.home');
-Route::get('/user/{name}/{id}', 'User\UserController@show')->name('user.profile');
+Route::get('/home', 'HomeController@home')
+    ->name('core.home');
+
+Route::get('/user/{name}/{id}', 'User\UserController@show')
+    ->name('user.profile')
+    ->where([
+        'name' => '[a-z0-9-]+',
+        'id' => '[0-9]+'
+    ]);
